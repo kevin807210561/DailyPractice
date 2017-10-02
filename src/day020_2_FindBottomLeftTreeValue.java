1b537816b8dcb01591c745730a238f5e7cb7d731
kevin807210561 <lzh15917953854@163.com>
2017-10-02 17:41:08 +0800

    day020

---
+++ src/day020_2_FindBottomLeftTreeValue.java
@@ -0,0 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class day020_2_FindBottomLeftTreeValue {
+    //①改进获取树的每一层节点个数的方法
+    //②可以每层从右向左遍历，这样遍历到最后一个节点即为目标节点
+    //③递归
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int length = 1, nextLength = 0, res = 0;
+        while(!queue.isEmpty()){
+            nextLength = 0;
+            res = queue.peek().val;
+            for (int i = 0; i < length; i++) {
+                TreeNode current = queue.peek();
+                if (current.left != null){
+                    queue.add(current.left);
+                    nextLength++;
+                }
+                if (current.right != null){
+                    queue.add(current.right);
+                    nextLength++;
+                }
+                queue.remove();
+            }
+            length = nextLength;
+        }
+        return res;
+    }
+}
