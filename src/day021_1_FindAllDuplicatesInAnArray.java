54cce5951864510c1ffc74c2866c52976e47e725
kevin807210561 <lzh15917953854@163.com>
2017-10-03 12:45:30 +0800

    day021

---
+++ src/day021_1_FindAllDuplicatesInAnArray.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class day021_1_FindAllDuplicatesInAnArray {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int num : nums) {
+            if (nums[Math.abs(num) - 1] < 0) res.add(Math.abs(num));
+            else nums[Math.abs(num) - 1] = -nums[Math.abs(num) - 1];
+        }
+        return res;
+    }
+}
---
+++ src/day021_2_QueueReconstructionByHeight.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class day021_2_QueueReconstructionByHeight {
+    //比较器可以尝试lambda表达式，更简洁
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new MyCompartor());
+        List<int[]> res = new LinkedList<>();
+        for (int[] p : people) {
+            res.add(p[1], p);
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
+
+class MyCompartor implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        int[] people1 = (int[])o1;
+        int[] people2 = (int[])o2;
+        int firstCompare = (Integer.valueOf(people2[0])).compareTo(Integer.valueOf(people1[0]));
+        return firstCompare == 0 ?
+                (Integer.valueOf(people1[1])).compareTo(Integer.valueOf(people2[1])) :
+                firstCompare;
+    }
+}
