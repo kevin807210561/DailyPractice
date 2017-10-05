79c619ec49e6a20d04dd48f99970dd281f16f971
kevin807210561 <lzh15917953854@163.com>
2017-10-05 23:17:15 +0800

    day002

---
+++ src/day002_1_mergeTrees.java
@@ -0,0 +1,32 @@
+public class day002_1_mergeTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null)
+            return null;
+
+        TreeNode res;
+        if (t1 == null) {
+            res = new TreeNode(t2.val);
+            res.left = mergeTrees(null, t2.left);
+            res.right = mergeTrees(null, t2.right);
+        } else if (t2 == null) {
+            res = new TreeNode(t1.val);
+            res.left = mergeTrees(t1.left, null);
+            res.right = mergeTrees(t1.right, null);
+        } else {
+            res = new TreeNode(t1.val + t2.val);
+            res.left = mergeTrees( t1.left, t2.left);
+            res.right = mergeTrees(t1.right, t2.right);
+        }
+        return res;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
