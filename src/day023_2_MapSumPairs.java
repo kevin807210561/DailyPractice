e4603cb68b4c7c85d6d0aa64ba5550202f2c1e56
kevin807210561 <lzh15917953854@163.com>
2017-10-05 23:23:17 +0800

    day023

---
+++ src/day023_2_MapSumPairs.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class day023_2_MapSumPairs {
+    private Map<String, Integer> map;
+
+    /** Initialize your data structure here. */
+    public day023_2_MapSumPairs() {
+        map = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        map.put(key, val);
+    }
+
+    public int sum(String prefix) {
+        int res = 0;
+        for (String key: map.keySet()) {
+            if (key.startsWith(prefix)) res += map.get(key);
+        }
+        return res;
+    }
+}
