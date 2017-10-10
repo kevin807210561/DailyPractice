d85d4a385b2f022540abd8b24e906d59c7da6a50
Kevin807210561 <lzh15917953854@163.com>
2017-10-10 20:21:02 +0800

    day024

--- src/day024_1_FirstUniqueCharacterInAString.java
+++ src/day024_1_FirstUniqueCharacterInAString.java
@@ -1,4 +1,5 @@
 public class day024_1_FirstUniqueCharacterInAString {
+    //可以有时间复杂度为O(n)的方法
     public int firstUniqChar(String s) {
         for (int i = 0; i < s.length(); i++) {
             if (s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))) return i;
